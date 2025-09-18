3dd03015c86ab7bd9f42fbc1685ab8bafbf795a5
Jose Ruiz <jose.ruizo@cecar.edu.co>
2025-09-17 22:36:58 -0500

    Version estable

--- db/init.sql
+++ db/init.sql
@@ -12,7 +12,7 @@ CREATE TABLE files (
     id INT AUTO_INCREMENT PRIMARY KEY,
     filename VARCHAR(255) NOT NULL,
     uploader_id INT,
-    upload_date DATE DEFAULT CURRENT_TIMESTAMP,
+    upload_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (uploader_id) REFERENCES users(id)
 );
 
